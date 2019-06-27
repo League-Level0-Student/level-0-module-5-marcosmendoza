b6ae100e764e00106890ffe6ff10d14363fe5355
ilabstaff <ilabstaff@172.16.116.67>
2019-06-26 19:28:55 -0700

    Code4life

--- src/_08_code_4_life/Code4Life.java
+++ src/_08_code_4_life/Code4Life.java
@@ -16,15 +16,23 @@ public static void main(String[] args) {
 	
 
 	// 1. Ask the user how many hours they spent coding this week.
-JOptionPane.showInputDialogue("how many hours have you spent cOdiNG this week");
+String hour = JOptionPane.showInputDialog("how many hours have you spent cOdiNG this week");
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+int hour2=Integer.parseInt(hour);
+if(hour2<=2) {
+	
+}
+	
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
 
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
-}
+		if(hour2>=5) {
+			
+		}
+		}
+
 	private static void playBatmanTheme() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
