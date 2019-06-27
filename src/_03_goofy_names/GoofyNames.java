bbf9af8603b4cdb8ddae5fcab39a63a4a9b68aa8
ilabstaff <ilabstaff@172.16.116.67>
2019-06-26 19:28:27 -0700

    Code4Life 	- GoofyNames

--- src/_03_goofy_names/GoofyNames.java
+++ src/_03_goofy_names/GoofyNames.java
@@ -11,26 +11,30 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String name = JOptionPane.showInputDialog("put in yall'er's name");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-
+		// Run your program to see that this works.
+		System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
+		// HINT: Use .length() to determine the number of characters in the String.
+		for (int i = 0; i < name.length(); i++) {
 
 			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
+			// using .substring(start, end
+			String temp = name.substring(i, i + 1);
+			// HINT: replace 'start' and 'end' to get String with character at i
+
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
+			// and lower case for ODD characters.
+			if (i % 2 == 0) {
+				// 6. ADD this String (containing 1 char) to the goofyName String
+				goofyName += temp.toUpperCase();
 
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
-
-		
-		// 7. Use pop-up to show user their Goofy name
-
+			} else if (i % 2 == 1)
+				;
+			// 7. Use pop-up to show user their Goofy name
+			goofyName += temp.toLowerCase();
+		}
+		JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
-
--- src/_08_code_4_life/Code4Life.java
+++ src/_08_code_4_life/Code4Life.java
@@ -3,19 +3,20 @@ package _08_code_4_life;
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
- */
-
-import java.applet.AudioClip;
+ */import java.applet.AudioClip;
 import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
+public static void main(String[] args) {
+	
 
 	// 1. Ask the user how many hours they spent coding this week.
-
+JOptionPane.showInputDialogue("how many hours have you spent cOdiNG this week");
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
 
@@ -23,7 +24,7 @@ public class Code4Life {
 	// they're a Code Ninja
 
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
-
+}
 	private static void playBatmanTheme() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
