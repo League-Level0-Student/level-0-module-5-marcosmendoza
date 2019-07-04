9e91c1daec4da62e102c358db95d3b7c1d852a5b
ilabstaff <ilabstaff@172.16.116.71>
2019-07-03 19:24:17 -0700

    Code4Life

---
+++ src/SkillPractice.java
@@ -0,0 +1,6 @@
+
+public class SkillPractice {
+public static void main(String[] args) {
+	
+}
+}
--- src/_08_code_4_life/Code4Life.java
+++ src/_08_code_4_life/Code4Life.java
@@ -12,32 +12,36 @@ import javax.swing.JApplet;
 import javax.swing.JOptionPane;
 
 public class Code4Life {
-public static void main(String[] args) {
-	
+	public static void main(String[] args) {
 
-	// 1. Ask the user how many hours they spent coding this week.
-String hour = JOptionPane.showInputDialog("how many hours have you spent cOdiNG this week");
-	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
-	// write code instead.
-int hour2=Integer.parseInt(hour);
-if(hour2<=2) {
-	
-}
-	
-	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
-	// they're a Code Ninja
+		// 1. Ask the user how many hours they spent coding this week.
+		String hour = JOptionPane.showInputDialog("how many hours have you spent cOdiNG this week");
+		// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
+		// write code instead.
+		int hour2 = Integer.parseInt(hour);
+		if (hour2 <= 2) {
+			JOptionPane.showMessageDialog(null,
+					"STOP WATCHING YOUTUBE AND RIGHT CODE INSTEad!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		}
 
-	// 4. If it is more than 5, call the method below to play the Batman theme song.
-		if(hour2>=5) {
-			
+		// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
+		// they're a Code Ninja
+
+		// 4. If it is more than 5, call the method below to play the Batman theme song.
+
+		else if (hour2 >= 3 && hour2 <= 5) {
+			JOptionPane.showMessageDialog(null, "yo yo yo yo yo youre a code ninja1!!!!!!!! woooo!!!!!");
+		} else if (hour2 > 5) {
+			playBatmanTheme();
+
 		}
-		}
+	}
 
 	private static void playBatmanTheme() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
 			sound.play();
-			Thread.sleep(60002);
+			Thread.sleep(600077452);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
